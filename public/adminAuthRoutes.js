01c4801f4d47e4a20c153d9525bce4ed3b5a4baa
leofranper37/sistema-gestao-igreja-ldfp <leopereita31@gmail.com>
2026-06-05 14:03:54 -0300

    altera

--- public/adminAuthRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminLoginController = require('../controllers/adminLoginController');
-
-router.post('/api/admin/login', adminLoginController.adminLogin);
-
-module.exports = router;
